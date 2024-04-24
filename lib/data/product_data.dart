55696ead9750e31e5fa83f96d20e195c41b263da
JOSE_CARLOS_BELTRAN_GAMEZ <a.21308051280319@cbtis128.edu.mx>
2024-04-24 15:09:18 +0000

    actualizado

---
+++ lib/data/product_data.dart
@@ -0,0 +1,19 @@
+import 'package:disenioBeltran/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/abrillantador.jpg",
+      title: "Abrillantador",
+      description:
+          "Abrillantador para piscina, sirve para mantener el agua cristalina"),
+  ProductModel(
+      img: "assets/images/algicida.jpg",
+      title: "Algicida",
+      description:
+          "Algicida para piscina, sirve para eliminar el alga de tu piscina"),
+  ProductModel(
+      img: "assets/images/cloro.jpg",
+      title: "Cloro",
+      description:
+          "Cloro para piscina, sirve para mantener el agua limpia y sin bacterias"),
+];
